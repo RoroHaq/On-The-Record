ddad96b950f8b8863b67b6f0370cec0d325ca35e
Safin Haque <safinhq13@gmail.com>
2024-10-22 11:27:24 -0400

    fix the response.body

--- server/test/api.test.js
+++ server/test/api.test.js
@@ -7,14 +7,14 @@ describe("/api/genre/:genre and ?year=Num Testing", () =>{
   it("Should return Country Object in 2017", async()=>{
     const response = await request(api).get("/api/genre/Country?year=2017")
 
-    expect(response.body()).to.deep.equal() //TODO Check the Object which will be determined later
+    expect(response.body).to.deep.equal() //TODO Check the Object which will be determined later
     expect(response.statusCode).to.equal(200);
   });
 
   it("Should return invalid query param", async()=>{
     const response = await request(api).get("/api/genre/Country?number=2017")
 
-    expect(response.body()).to.deep.equal({error: "Invalid query Parameter"})
+    expect(response.body).to.deep.equal({error: "Invalid query Parameter"})
     expect(response.statusCode).to.equal(404);
   });
 });
@@ -22,13 +22,14 @@ describe("/api/genre/:genre and ?year=Num Testing", () =>{
 describe("/api/streams/top/:year Tests", () =>{
   it("Should check if the top years array has the genre data during 2016", async () =>{
     const response = await request(api).get("/api/streams/top/2016")
-    response.body().forEach(genre =>{
+    response.body.forEach(genre =>{
       expect(genre.year).to.equal(2016)
     })
   })
 
   it("Should check if the top years array has the genre data during 2016", async () =>{
     const response = await request(api).get("/api/streams/top/2016")
-    expect(response.body()[0].rank).to.equal(1)
+    expect(response.body[0].rank).to.equal(1)
+    expect(response.body[0].year).to.equal(2016)
   })
 });
