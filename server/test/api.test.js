b360ee56a1aa6cf77acf0de02e6dc365a8e39f99
Safin Haque <safinhq13@gmail.com>
2024-10-22 11:23:02 -0400

    make unit test for top streams in 2016

--- server/test/api.test.js
+++ server/test/api.test.js
@@ -9,12 +9,21 @@ describe("/api/genre/:genre and ?year=Num Testing", () =>{
 
     expect(response.body()).to.deep.equal() //TODO Check the Object which will be determined later
     expect(response.statusCode).to.equal(200);
-  })
+  });
 
   it("Should return invalid query param", async()=>{
     const response = await request(api).get("/api/genre/Country?number=2017")
 
     expect(response.body()).to.deep.equal({error: "Invalid query Parameter"})
     expect(response.statusCode).to.equal(404);
+  });
+});
+
+describe("/api/streams/top/:year Tests", () =>{
+  it("Should check if the top years array has the genre data during 2016", async () =>{
+    const response = await request(api).get("/api/streams/top/2016")
+    response.forEach(genre =>{
+      expect(genre.year).to.equal(2016)
+    })
   })
-})
+});
