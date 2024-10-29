25aaf4c7663fac63b5018501a55fefbdc4e26363
NathanBluetornado <87149868+NathanBluetornado@users.noreply.github.com>
2024-10-29 10:46:01 -0400

    Create function to map spotify data to the needed object format

--- server/data/data-fetch.mjs
+++ server/data/data-fetch.mjs
@@ -1,6 +1,6 @@
 import fs from 'node:fs/promises'
 import neat from 'neat-csv'
-async function getBillBoardData(){
+async function getBaseBillBoardData(){
   try{
     const response = await fs.readFile('charts.csv')
     
@@ -12,9 +12,9 @@ async function getBillBoardData(){
   }
 }
 
-async function filterBillBoardData(){
+async function getFilteredBillBoardData(){
   try{
-    const songs = await getBillBoardData()
+    const songs = await getBaseBillBoardData()
     const startDate = new Date('2010-01-01').toJSON().slice(0, 10);
     const endDate = new Date('2021-12-31').toJSON().slice(0, 10);
     return songs.filter(song => song.date >= startDate && song.date <= endDate)
@@ -23,7 +23,7 @@ async function filterBillBoardData(){
   }
 }
 
-async function getSpotifyData() {
+async function getBaseSpotifyData() {
   try{
     const response = await fs.readFile('spotify_full_list.csv')
     
@@ -35,13 +35,65 @@ async function getSpotifyData() {
   }
 }
 
-async function filterSpotifyData(){
+async function getFilteredSpotifyData(){
   try{
-    const songs = await getSpotifyData()
+    const songs = await getBaseSpotifyData()
     const startDate = new Date('2010-01-01').getFullYear()+1
     const endDate = new Date('2021-12-31').getFullYear()
 
     return songs.filter(song => song.year >= startDate && song.year <= endDate)
+      .filter(song => song.main_genre !== '')
+  }catch(Error){
+    console.log(Error)
+  }
+}
+
+
+async function getMappedSpotifyData(){
+  try{
+    const songs = await getFilteredSpotifyData()
+
+    const deriveNewFields = (song) => {
+      //.title provides the title of the song without the artist
+      const offset = 3
+      song.title = song['Artist and Title'].slice(song.Artist.length + offset, song['Artist and Title'].length)
+
+      //.genre renames the .main_genre field
+      song.genre = song.main_genre
+      return song
+    }
+
+    const removeUnneededFields = (song) => {
+      delete song.genres
+      delete song.main_genre
+      delete song.first_genre
+      delete song.second_genre
+      delete song.third_genre
+      delete song.Daily
+      delete song.id
+      delete song['Artist and Title']
+      return song
+    }
+
+    const makeFieldsLowerCase = (song) => {
+      song.artist = song.Artist
+      song.streams = song.Streams
+      delete song.Artist
+      delete song.Streams
+      return song
+    }
+
+    const roundYearField = (song) => {
+      song.year = parseInt(song.year)
+      return song
+    }
+
+    return songs//.filter(song => song.Artist === 'Kendrick Lamar')
+                .map(deriveNewFields)
+                .map(removeUnneededFields)
+                .map(makeFieldsLowerCase)
+                .map(roundYearField)
+
   }catch(Error){
     console.log(Error)
   }
