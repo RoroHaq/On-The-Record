9bf5a4ed06f4feab9eebcfaa9199bcddf58e30a5
Safin Haque <safinhq13@gmail.com>
2024-11-09 13:55:45 -0500

    fix client lint and add our last names

--- client/src/components/Credits.jsx
+++ client/src/components/Credits.jsx
@@ -4,13 +4,12 @@ export default function Credits(){
 
   return<>
     <section>
-      <p>Website Made by Safin, Nathan & Minh</p>
+      <p>Website Made by Minh Phu Ngo, Nathan Bokobza & Safin Haque</p>
       <h3>Credits:</h3>
       <ul>
-        <li>Dataset 1: <a href="https://www.kaggle.com/datasets/irynatokarchuk/top-streamed-spotify-songs-by-year-2010-2023" target="_blank">Top Streamed Spotify Songs by year 2010-2023</a></li>
-        <li>Dataset 2: <a href="https://www.kaggle.com/datasets/dhruvildave/billboard-the-hot-100-songs">Billboard "The Hot 100" Songs</a></li>
+        <li>Dataset 1: <a href='https://www.kaggle.com/datasets/irynatokarchuk/top-streamed-spotify-songs-by-year-2010-2023' target='_blank'>Top Streamed Spotify Songs by year 2010-2023</a></li>
+        <li>Dataset 2: <a href='https://www.kaggle.com/datasets/dhruvildave/billboard-the-hot-100-songs' target='_blank'>Billboard &apos;The Hot 100&apos; Songs</a></li>
       </ul>
-      <p></p>
     </section>
   </>
 }
