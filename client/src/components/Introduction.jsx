c192edd3438e5be5a26147a41c5f64eb27e986cc
NathanBluetornado <87149868+NathanBluetornado@users.noreply.github.com>
2024-11-10 15:34:48 -0500

    Add Introduction Composoble and style title

---
+++ client/src/components/Introduction.jsx
@@ -0,0 +1,13 @@
+
+/**
+ * Provides an introduction to the website along with the title and logo
+ * @returns title and introduction
+ */
+export default function Introduction() {
+  
+  return (
+    <div id="title-box">
+      <h1 className="title">On the Record</h1>
+    </div>
+  )
+}
