e0b4ed2a03a5c2aeb5649c9159969318db5c665e
NathanBluetornado <87149868+NathanBluetornado@users.noreply.github.com>
2024-11-12 20:46:29 -0500

    Enable fetching of genres from all years

--- client/src/components/GenreRatioChart.jsx
+++ client/src/components/GenreRatioChart.jsx
@@ -1,6 +1,11 @@
 import { useState, useEffect } from 'react';
-import { Bar } from 'react-chartjs-2';
+import { Line } from 'react-chartjs-2';
 import { Chart as ChartJS } from 'chart.js/auto'
+
+/**
+ * Years which the database has access to
+ */
+const genreYears = [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021];
 
 /**
  * Fetches genre objects for specific year (2017 for now) and ranks their popularity on a table
@@ -8,75 +13,88 @@ import { Chart as ChartJS } from 'chart.js/auto'
  */
 export default function GenreRatioChart() {
   
-  const [genreData, setGenreData] = useState( [] );
+  const [genreData, setGenreData] = useState([]);
 
   useEffect( () => {    
-    async function fetchMostStreamedGenre(year) {
-      const genres = await fetch(`/api/streams/top/${year}`);
+    async function fetchGenre(year) {
+      const genres = await fetch(`/api/genre/all/${year}`);
       const json = await genres.json();
-      // setGenreData( gd => {
-      //   const newArr = gd;
-      //   newArr.push(json.data);
-      //   return newArr;
-      // });
-      setGenreData(json.data)
+      return await json.data;
     }
-    fetchMostStreamedGenre(2013);
+    
+    async function fetchGenreDataForAllYears() {
+      //TODO: temporary solution
+      const retirevedGenreData = await Promise.all([
+        // genreYears.map(fetchGenre)
+        fetchGenre(2010),
+        fetchGenre(2011),
+        fetchGenre(2012),
+        fetchGenre(2013),
+        fetchGenre(2014),
+        fetchGenre(2015),
+        fetchGenre(2016),
+        fetchGenre(2017),
+        fetchGenre(2018),
+        fetchGenre(2019),
+        fetchGenre(2020),
+        fetchGenre(2021)
+      ]);
+
+      setGenreData(retirevedGenreData)
+    }
+
+    fetchGenreDataForAllYears()
   }, []);
 
-  useEffect( () =>
-    {
-      setChartData({
-        labels: genreData.map((data) => data.genre), 
-        datasets: [
-          {
-            label: "Genre Data",
-            data: genreData.map((data) => data.totalStreams),
-            backgroundColor: [
-              "rgba(75,192,192,1)",
-              "#ecf0f1",
-              "#50AF95",
-              "#f3ba2f",
-              "#2a71d0"
-            ],
-            borderColor: "black",
-            borderWidth: 5
-          }
-        ]
-      })
-    }, [genreData] );
-
-  // const data = [
-  //   { year: 2010, count: 10 },
-  //   { year: 2011, count: 20 },
-  //   { year: 2012, count: 15 },
-  //   { year: 2013, count: 25 },
-  //   { year: 2014, count: 22 },
-  //   { year: 2015, count: 30 },
-  //   { year: 2016, count: 28 }
-  // ];
-
-  console.log(genreData)
-
-  const [chartData, setChartData] = useState({});
+  let data = (genreData.length > 0) ? {
+    labels: genreYears,
+    datasets: genreYears.map( (year, index) => 
+      [
+        {
+          data: [genreData[index].map((item) => item.totalStreams)],
+          backgroundColor: [
+            "#FF6384",
+            "#36A2EB",
+            "#FFCE56",
+            "#4BC0C0",
+            "#9966FF",
+            "#FF9F40",
+            "#C9CBCF",
+            "#8AC926",
+            "#FF6F59",
+          ],
+          hoverOffset: 4,
+          "id": year,
+          "label": "Purchase amount (USD)",
+          "yAxisID":"left"
+        }
+      ]
+    )
+  } : null
 
   return (
     <div className="chart-container" id="ha">
       <h2 id="haha" style={{ textAlign: "center" }}>Bar Chart</h2>
-      <Bar
-        data={chartData}
-        options={{
-          plugins: {
-            title: {
-              display: true,
-              text: "2012 Streaming Data"
-            },
-            legend: {
-              display: false
+      
+      {
+      data ? 
+        <Line
+          data={data}
+          options={{
+            plugins: {
+              title: {
+                display: true,
+                text: "2012 Streaming Data"
+              },
+              legend: {
+                display: false
+              }
             }
-          }
-        }}
-      />
+          }}
+        />
+      : <p>Loading</p>
+      }
+      
     </div>
   );
 }
