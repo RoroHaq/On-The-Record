fe641c02bfddc3bfea9cf939c15729debb872ceb
Safin Haque <safinhq13@gmail.com>
2024-11-12 20:34:05 -0500

    implement useEffect for WeeksOnBoardChart Component

--- client/src/components/WeeksOnBoardChart.jsx
+++ client/src/components/WeeksOnBoardChart.jsx
@@ -2,17 +2,33 @@ import Chart from 'chart.js/auto'
 import { useState } from "react";
 import { Bar } from 'react-chartjs-2'
 
-export default function WeeksOnboardchart({weeksOnBoardData}){
-
+export default function WeeksOnboardchart(){
+  const [weeklyPlacementData, setweeklyPlacementData] = useState([])
+  const [year, setYear] = useState(2010);
   const [BarChartData, setBarChartData] = useState({
-    labels : weeksOnBoardData.map((data) => data.genre),
+    labels : weeklyPlacementData.map((data) => data.genre),
     datasets: [
       {
       label : "Genres with Most Weeks on Board",
-      data : weeksOnBoardData.map((data) => data.TotalWeeklyPlacement)
+      data : weeklyPlacementData.map((data) => data.TotalWeeklyPlacement)
     }
     ] 
   });
+
+  useEffect( () => {    
+    async function fetchMostStreamedGenre(year) {
+      
+      try {
+        const response = await fetch(`/api/genres/all/${year}`);
+        const json = await response.json();
+        setweeklyPlacementData(json.data); 
+      } catch (error) {
+        console.error("Error fetching genre data:", error);
+      }
+    }
+
+    fetchMostStreamedGenre(year);
+  }, [year]);
 
   return <>
     <div>
