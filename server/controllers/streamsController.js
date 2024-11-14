f713807970a4b419be52668df68c8e7e457e9973
Safin Haque <safinhq13@gmail.com>
2024-11-13 23:58:59 -0500

    add JSDocs for middleware in streamsController.js

--- server/controllers/streamsController.js
+++ server/controllers/streamsController.js
@@ -1,10 +1,13 @@
 import { db } from '../db/db.js'
 
 /**
+ * This method process the user request when putting the 
+ * /api/streams/top/:year as it sends the user an object which includes a list
+ * of songs from the specified year. Otherwise it will throw an Error.
  * 
- * @param {Object} req 
- * @param {Object} res 
- * @param {Function} next 
+ * @param {Object} req The User's request (Query Params/Inputs)
+ * @param {Object} res The Response of the Middleware
+ * @param {Function} next Use to call the next succeeding Middleware after this
  */
 export async function getMostStreamedGenresByYear(req, res, next){
   const year = parseInt(req.params.year);
