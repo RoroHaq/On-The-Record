d959ebef5e279ee856bdae06e6ba2e4efcc6d18d
Safin Haque <safinhq13@gmail.com>
2024-11-14 13:27:25 -0500

    add import Chart that cause issues

--- client/src/components/WeeksOnBoardChart.jsx
+++ client/src/components/WeeksOnBoardChart.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { Bar } from 'react-chartjs-2'
+import Chart from "chart.js/auto";
 
 export default function WeeksOnboardchart(){
   const [weeklyPlacementData, setweeklyPlacementData] = useState([])
