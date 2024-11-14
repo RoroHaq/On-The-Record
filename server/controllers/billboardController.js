d74ae3636ab638ea4fa18ec3120b484d7f45df7e
Safin Haque <safinhq13@gmail.com>
2024-11-13 23:52:53 -0500

    include JSDocs for functions in billboardController.js

--- server/controllers/billboardController.js
+++ server/controllers/billboardController.js
@@ -1,5 +1,14 @@
 import { db } from '../db/db.js'
 
+/**
+ * This function is used to validate the Query param given in the endpoint
+ * /api/billboard, if its valid or a query param is not given, it will go to the next function 
+ * to process it, otherwise it goes to the error handling middlewear to respond with an Error.
+ * 
+ * @param {Object} req The User's request (Query Params/Inputs)
+ * @param {Object} res The Response of the Middleware
+ * @param {Function} next Use to call the next succeeding Middleware after this
+ */
 export function validateBillboardQueryParam(req, res, next){
   try{
     if(Object.keys(req.query).length > 0 && !req.query.year){
@@ -13,6 +22,14 @@ export function validateBillboardQueryParam(req, res, next){
   }
 }
 
+/**
+ * This will go through the request and respond with a json representation
+ * of the data, which is a list of top 100 songs on the billboard of a certain year or all years.
+ * 
+ * @param {Object} req The User's request (Query Params/Inputs)
+ * @param {Object} res The Response of the Middleware
+ * @param {Function} next Use to call the next succeeding Middleware after this
+ */
 export async function getTopBillBoardSongs(req, res, next){
   const year = parseInt(req.query.year, 10);
   try{
