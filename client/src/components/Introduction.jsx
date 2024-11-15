30dbdcbf7df0b9f34000b81cda3e78075507a23e
NathanBluetornado <87149868+NathanBluetornado@users.noreply.github.com>
2024-11-14 20:12:58 -0500

    Fix Credits and semantics

--- client/src/components/Introduction.jsx
+++ client/src/components/Introduction.jsx
@@ -6,11 +6,11 @@
 export default function Introduction() {
   
   return (
-    <div id="title-box">
+    <header id="title-box">
       <h1 className="title">On the Record</h1>
       <h3>An <span className="on-beat">on-beat</span> data driven story about music genre shifts in the Billboard top 100 and Spotify streaming numbers between 2010 and 2021</h3>
       <hr></hr>
       <p>Scroll to see the story, spinning the record can be used to scroll faster.</p>
-    </div>
+    </header>
   )
 }
