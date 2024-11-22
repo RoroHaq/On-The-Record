a5445968e8f1d9bef88b213456fff7760ea327e6
SafinHQ13 <safin.haque@dawsoncollege.qc.ca>
2024-11-22 16:22:01 +0000

    fix production dependency

--- server/bin/www.js
+++ server/bin/www.js
@@ -3,9 +3,13 @@ import app from '../app.js';
 import { db } from '../db/db.js';
 const port = process.env.PORT || 3000;
 
+
+
 (async () => {
   try {
+	  console.log("hi!");
     await db.connect('OnTheRecordDB', 'songs');
+	  console.log("bye!");
   } catch (e) {
     console.error('could not connect');
     // eslint-disable-next-line no-console
