a7fd675465e3a8452438ee37c1e748802562ef16
Minardi299 <phu.ngo@outlook.com>
2024-11-24 19:53:03 -0500

    assign dedicate color for each genre

--- client/src/components/GenreStreamsChart.jsx
+++ client/src/components/GenreStreamsChart.jsx
@@ -7,6 +7,19 @@ export default function GenreStreamsChart(){
   const [genreData, setGenreData] = useState( [] );
 
   const textColour = "#FAF9F6"  
+  const genreColors = {
+    "Pop": "#36A2EB",
+    "Hip-Hop/Rap": "#9966FF", 
+    "Rock": "#FF6384",
+    "R&B/Soul": "#5F5F79",
+    "World/Traditional": "#FF9F40",
+    "Electronic/Dance": "#8AC926",
+    "Indie/Alternative": "#FF6F59",
+    "Metal": "#FFCE56",
+    "Classical/Orchestral": "#4BC0C0",
+    "Other": "#C9CBCF",
+  };
+
   const options = {
     color: textColour,
     responsive: true,
@@ -75,17 +88,7 @@ export default function GenreStreamsChart(){
     datasets: [
       {
         data: genreData.map((item) => item.totalStreams),
-        backgroundColor: [
-          "#FF6384",
-          "#36A2EB",
-          "#FFCE56",
-          "#4BC0C0",
-          "#9966FF",
-          "#FF9F40",
-          "#C9CBCF",
-          "#8AC926",
-          "#FF6F59",
-        ],
+        backgroundColor: genreData.map((item) => genreColors[item.genre] || "#000000"),
         hoverOffset: 4,
       },
     ]
