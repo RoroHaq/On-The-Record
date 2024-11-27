03e70e92c88c96d47329e2326f74fc8e0f03a176
NathanBluetornado <87149868+NathanBluetornado@users.noreply.github.com>
2024-11-26 20:37:43 -0500

    Repair heading element order and css rules

--- client/src/components/Credits.jsx
+++ client/src/components/Credits.jsx
@@ -2,7 +2,7 @@ export default function Credits(){
 
   return <footer className="transparent-background">
       <h2><span className="on-beat">Data mixed</span> for you by Nathan Bokobza, Safin Haque, & Minh Phu Ngo</h2>
-      <h3>Credits:</h3>
+      <h2>Credits:</h2>
       <ul>
         <li>Dataset 1: <a href='https://www.kaggle.com/datasets/irynatokarchuk/top-streamed-spotify-songs-by-year-2010-2023' target='_blank'>Top Streamed Spotify Songs by year 2010-2023</a></li>
         <li>Dataset 2: <a href='https://www.kaggle.com/datasets/dhruvildave/billboard-the-hot-100-songs' target='_blank'>Billboard &apos;The Hot 100&apos; Songs</a></li>
